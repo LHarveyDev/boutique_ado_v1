a24ac44fe044ee25ceff434fa2e6b2ef23b00951
LHarveyDev <lucyanneharvey@hotmail.com>
2024-02-28 16:54:40 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_live_51OoMhEGrkEJ9fJrMoVoevgI02wBpiLPN4RLkO7h0wF2lbNLq0oHRm7cTQaqq3cQXKGxNBKOxJacxYNscS8wOh1Ed00ZLWx2NSQ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
